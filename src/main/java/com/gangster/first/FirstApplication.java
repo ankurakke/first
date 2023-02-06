aa27b9fb9c2d352c2cec3852394a95f9d6117fdd
ankurakke <49191191+ankurakke@users.noreply.github.com>
2023-02-06 15:42:05 +0530

    removed hello message from main controller

--- src/main/java/com/gangster/first/FirstApplication.java
+++ src/main/java/com/gangster/first/FirstApplication.java
@@ -9,7 +9,6 @@ public class FirstApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
-		System.out.println("Hello World");
 	}
 
 	@Override
